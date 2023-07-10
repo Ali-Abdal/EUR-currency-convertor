50fd900f10ce6272be66b3e71e8ce1557f502a3e
Ali Abdal <109912475+Ali-Abdal@users.noreply.github.com>
2023-07-10 16:14:29 +0100

    Update convertor_no_ui.py

--- convertor_no_ui.py
+++ convertor_no_ui.py
@@ -1,26 +1,22 @@
 import requests
 
-
-base_url = 'http://api.exchangeratesapi.io/v1'
-endpoint = 'latest'
-access_key = 'access_key=8ee28eec0c01266b9c8d9f53894669ce' # each acc has differant key
+base_url = 'http://api.exchangeratesapi.io/v1/latest'
+access_key = 'access_key=8ee28eec0c01266b9c8d9f53894669ce' 
 currency = input('Convert from EUR to (currency):\n> ')
 quantity = float(input(f"How much EUR do you want to convert:\n> "))
 
-full_url = base_url + '/' + endpoint + '?' + access_key + '&symbols=' + currency
+full_url = base_url + '?' + access_key + '&symbols=' + currency
 
 response = requests.get(full_url)
 
-# checks if the request is successful it prints the output if there is an error it prints a friendly error message
 if response.ok:
     data = response.json()
     rate = data['rates'][currency]
 
     result = quantity * rate
     
-    print(f"\n{quantity} EUR is equal to {round(result,2)} {currency}, based upon exchane rates on {data['date']}")
+    print(f"\n{quantity} EUR is equal to {round(result,2)} {currency}")
 
 else:
     print(f"Error {response.status_code}")
     print(response.json()['error']['message'])
-
